be7b46891eee38834962ac6349ba8920d605357b
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-02-20 09:33:14 +0000

    added BASE_DIR templates folder

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration, user_profile
-from accounts import url_reset
+from . import urls_reset
+from .views import index, register, profile, logout, login
 
 urlpatterns = [
-    url(r'^logout/', logout, name="logout"),
-    url(r'^login/', login, name="login"),
-    url(r'^register/', registration, name="registration"),
-    url(r'^profile/', user_profile, name="profile"),
-    url(r'^password-reset/', include(url_reset))
+    url(r'^register/$', register, name='register'),
+    url(r'^profile/$', profile, name='profile'),
+    url(r'^logout/$', logout, name='logout'),
+    url(r'^login/$', login, name='login'),
+    url(r'^password-reset/', include(urls_reset)),
 ]
