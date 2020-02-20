902c36aca8c13ec87d278ba236770abef829a00c
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-02-20 10:49:37 +0000

    addex cart_contents to templates in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
