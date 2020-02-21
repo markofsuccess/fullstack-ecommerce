4e8d280a927bee86c4b1348c03eac6a0775a51b9
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-02-21 14:48:54 +0000

    added import env to settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from os import path
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
