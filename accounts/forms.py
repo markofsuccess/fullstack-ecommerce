c0168313f38e16abd88206547678cecfcd33f1ae
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-02-27 10:19:48 +0000

    changed login form to be username_or_email the same as in views

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,31 +5,26 @@ from django.core.exceptions import ValidationError
 
 
 class UserLoginForm(forms.Form):
-    """Form to be used to log users in"""
-
-    username = forms.CharField()
+    username_or_email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(UserCreationForm):
-    """Form used to register a new user"""
-
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label="Password Confirmation",
-        widget=forms.PasswordInput)
-    
+        label='Password Confirmation',
+        widget=forms.PasswordInput
+    )
+
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
-    
+        fields = ['username', 'email', 'password1', 'password2']
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
         if User.objects.filter(email=email).exclude(username=username):
-            raise forms.ValidationError(u'Email address must be unique')
+            raise forms.ValidationError(u'Email addresses must be unique.')
         return email
 
     def clean_password2(self):
@@ -37,9 +32,9 @@ class UserRegistrationForm(UserCreationForm):
         password2 = self.cleaned_data.get('password2')
 
         if not password1 or not password2:
-            raise ValidationError("Please confirm your password")
-        
+            raise ValidationError("Password must not be empty")
+
         if password1 != password2:
-            raise ValidationError("Passwords must match")
-        
+            raise ValidationError("Passwords do not match")
+
         return password2
