cd32a5c4f40c89e5693c353e775afb075613e4ee
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-03-04 13:13:18 +0000

    changed max_digits decimals for Product price, and migrated

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     image = models.ImageField(upload_to='images')
   
     def __str__(self):
