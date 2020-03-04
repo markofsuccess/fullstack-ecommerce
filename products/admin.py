5ed8be4ab72a5229c9bccf0d9a1ed4342d442a2f
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-03-04 11:49:50 +0000

    added product_image class to models, updated views.py and admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product
-from .models import product_details
+from .models import product_image
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(product_image)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-from .models import product_details
+from .models import product_image
 
 # Create your views here.
 def all_products(request):
@@ -8,6 +8,6 @@ def all_products(request):
     return render(request, "products.html", {"products": products})
 
 def details(request, id):
-    details = product_details.objects.filter(product_id = id)
-    product= Product.objects.get(id=id)
+    details = product_image.objects.filter(product_id = id)
+    product = Product.objects.get(id=id)
     return render(request, "product_details.html", {'details': details, 'product': product})
