76343d97b123c73e311d16ed12233900e2fdf1c1
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-03-06 10:35:55 +0000

    changed debug to False for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get(''),'127.0.0.1','localhost']
+ALLOWED_HOSTS = [os.environ.get(''),'127.0.0.1','localhost','django-markus-ecommerce.herokuapp.com/']
 
 
 # Application definition
