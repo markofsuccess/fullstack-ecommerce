a20bb30b17348565bdf228e2498b2a18fa5cf1e4
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-03-06 09:09:42 +0000

    added AWS access key id and object parameters

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,9 +138,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000'
+}
+
 AWS_STORAGE_BUCKET_NAME = 'fullstack-ecommerce-markus'
 AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
