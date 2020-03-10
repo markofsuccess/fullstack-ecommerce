6cd156e3859cbafbd9d2c8436c011e8cfb93ea66
Markus Turesson Sjöman <markussjoman1@gmail.com>
2020-03-10 10:44:43 +0000

    changed allowed hosts settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get(''),'127.0.0.1','localhost','django-markus-ecommerce.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','django-markus-ecommerce.herokuapp.com']
 
 
 # Application definition
